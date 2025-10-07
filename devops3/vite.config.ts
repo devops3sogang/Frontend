ccbba855dec04ecbaf6db513026f01c94e17abf6
Hgray1591 <Hgray1591@users.noreply.github.com>
2025-10-07 12:53:40 +0900

    vite.config.ts 수정

--- devops3/vite.config.ts
+++ devops3/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8282,
+    host: true, // 외부 접속 허용
+    open: true, // 서버 시작 시 자동으로 브라우저 열기
+  },
 })
